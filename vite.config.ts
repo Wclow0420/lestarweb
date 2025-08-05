5267bc6f0dfffe4f27d36e8698e01d20e79fc097
Wclow0420 <wclow0420@gmail.com>
2025-08-05 16:08:19 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base: "/<REPO_NAME>/", // Replace <REPO_NAME> with your GitHub repo name
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
